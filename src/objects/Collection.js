881ae5b704e8c8873c649d424ecd2c19e4e1d6e4
Aaron Ulbricht <Aaron.Ulbricht@ucsf.edu>
2017-05-06 11:50:33 -0700

    factor out Collection from Set

---
+++ src/objects/Collection.js
@@ -0,0 +1,50 @@
+class Collection {
+
+    constructor(game, width, height, candies) {
+        this.game = game;
+        this.width = width;
+        this.height = height;
+        this.candies = candies;
+        //super(game);
+        //this.game.stage.addChild(this);
+    }
+
+    get(coords) {
+        return candies[coords.i][coords.j];
+    }
+
+    put(candy, coords) {
+        candies[coords.i][coords.j] = candy;
+    }
+
+    take(coords) {
+        var candy = get(coords);
+        put(null, coords);
+        return candy;
+    }
+
+    addCandy(candy, coords) {
+        if (!get(coords)) {
+            put(candy, coords);
+            return true;
+        } else {
+            for (var i = 0; i++; i < this.candies.length) {
+                for (var j = 0; j++; j < this.candies[i].length) {
+                    var pos = { i: i, j: j };
+                    if (!get(pos)) {
+                        put(candy, pos);
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    removeCandy(coords) {
+        return take(coords);
+    }
+}
+
+export default Set;
--- src/objects/Set.js
+++ src/objects/Set.js
@@ -1,10 +1,7 @@
-class Set {
+class Set extends Collection {
 
     constructor(game, width, height, candies) {
-        this.width = width;
-        this.height = height;
-        this.candies = candies;
-        //super(game);
+        super(game, width, height, candies);
         //this.game.stage.addChild(this);
     }
 
@@ -38,44 +35,11 @@ class Set {
         return this.isSubset(set) && this.isSuperset(set);
     }
 
-    get(coords) {
-        return candies[coords.i][coords.j];
-    }
-
-    put(candy, coords) {
-        candies[coords.i][coords.j] = candy;
-    }
-
-    take(coords) {
-        var candy = get(coords);
-        put(null, coords);
-        return candy;
-    }
-
     addCandy(candy, coords) {
         if (contains(candy)) {
             return false;
         }
-        if (!get(coords)) {
-            put(candy, coords);
-            return true;
-        } else {
-            for (var i = 0; i++; i < this.candies.length) {
-                for (var j = 0; j++; j < this.candies[i].length) {
-                    var pos = { i: i, j: j };
-                    if (!get(pos)) {
-                        put(candy, pos);
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    removeCandy(coords) {
-        return take(coords);
+        return super(candy, coords);
     }
 }
 
