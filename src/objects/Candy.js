6de64027e06c309cde4107b453eaf6d7fdbd7380
Aaron Ulbricht <Aaron.Ulbricht@ucsf.edu>
2017-05-06 19:13:26 -0700

    bring to top when dragging

--- src/objects/Candy.js
+++ src/objects/Candy.js
@@ -12,15 +12,23 @@ var colorMap = function(color) {
 class Candy extends Phaser.Sprite {
     constructor(game, type, color, x, y, size) {
         super(game, x, y, type, colorMap(color));
+        this.anchor.setTo(0.5, 0.5);
         this.type = type;
         this.color = color;
         game.add.existing(this);
+        if (type === 'jollyRancher') {
+            this.angle += 45;
+        }
+
+        var max = Math.max(this.width, this.height);
+        var scale = size / max;
+        this.x += size / 2;
+        this.y += size / 2;
+
+        this.scale.setTo(scale, scale);
 
         this.inputEnabled = true;
-        this.input.enableDrag();
-        this.height = size;
-        if (this.width > size);
-        this.width = size;
+        this.input.enableDrag(true, true);
     }
 
     isEqual(candy) {
